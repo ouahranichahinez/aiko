b66182cb257c95e53849373393815e5579ad36ca
cchahinez <chahinez.work@gmail.com>
2022-07-21 11:39:17 +0100

    s

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+
+const { MongoClient } = require("mongodb");
+
+const uri =
+    "mongodb+srv://xene:HHez1wpLFRCUJCqw@essay-cluster.ah07px7.mongodb.net/sample_mflix?retryWrites=true&w=majority";
+
+// use the express-static middleware
+app.use(express.static("public"));
+
+// define the first route
+app.get("/api/movie", async function (req, res) {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+
+        const database = client.db("sample_mflix");
+        const collection = database.collection("movies");
+
+        // Query for a movie that has the title 'Back to the Future'
+        const query = { genres: "Comedy", poster: { $exists: true } };
+        const cursor = await collection.aggregate([
+            { $match: query },
+            { $sample: { size: 1 } },
+            {
+                $project: {
+                    title: 1,
+                    fullplot: 1,
+                    poster: 1,
+                },
+            },
+        ]);
+
+        const movie = await cursor.next();
+
+        return res.json(movie);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+});
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
